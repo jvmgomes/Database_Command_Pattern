1957e3ad8e25eea88f30fe306674654d3843962e
PedroHenriqueNovello <phnporto@gmail.com>
2026-06-23 16:52:09 -0300

    feat:Implementação da classe Server

---
+++ src/Command/Server.cs
@@ -0,0 +1,38 @@
+namespace src.Command;
+
+public class Server
+{
+    private Dictionary<int, string> db = new Dictionary<int, string>();
+    private Dictionary<string, ICommand> cmds = new Dictionary<string, ICommand>();
+
+    public Server()
+    {
+        initCommands();
+    }
+
+    private void InitCommands()
+    {
+        cmds.Add("new", new NewCommand(db));
+        cmds.Add("del", new DeleteCommand(db));
+        cmds.Add("delete", new DeleteCommand(db));
+        cmds.Add("get", new GetCommand(db));
+        cmds.Add("all", new AllCommand(db));
+    }
+
+    public void Service (string cmd, object data)
+    {
+        if (cmds.TryGetValue(cmd, out ICommand c))
+        {
+            c.Execute(data);
+
+        }
+
+        else
+        {
+            Console.WriteLine($"Comando '{cmd}' não reconhecido pelo Server");
+        }
+    }
+    
+
+
+}
